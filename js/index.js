ee81240de76cf24b06e10099ca7fc3fbfb04db7e
Douglas C <douglas218@gmail.com>
2020-05-18 09:14:01 -0300

    exit popup responsiva, mouse pointer

--- js/index.js
+++ js/index.js
@@ -1,15 +1,18 @@
+/* 
+ * EXIT POP-UP 
+ */
 function exitPopup(event) {
     // Remove this event listener
     document.removeEventListener("mouseout", exitPopup);
   
     // Show the popup
     document.getElementById("popup").style.display = "flex";
-  }
-  
-  document.addEventListener("mouseout", exitPopup);
+}
 
+/* Listener para abrir a exit popup no evento "mouseout" */
+document.addEventListener("mouseout", exitPopup);
 
-//para fechar o popup quando clicar no X
-  document.getElementById("close-popup").addEventListener("click", function(){
+/* Para fechar o popup quando clicar no X */
+document.getElementById("close-popup").addEventListener("click", function(){
     document.getElementById("popup").style.display = "none";
-  });
+});
